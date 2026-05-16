9ce7aa94d7fbf4ff4de230f9f2988fa91bb2ce7a
Mike Kaipis <kaipismike1@gmail.com>
2026-05-16 09:29:08 +0300

    gazebo simulation udpate

--- src/simulation/launch/gazebo.launch.py
+++ src/simulation/launch/gazebo.launch.py
@@ -1,62 +1,48 @@
 import os
 
-from ament_index_python import get_package_share_directory
+from ament_index_python.packages import get_package_share_directory, PackageNotFoundError
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
-from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution, TextSubstitution
-from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
-    # 1. Path to Gazebo World
-    world_path = PathJoinSubstitution([
-        FindPackageShare('simulation'), 'worlds', 'drone_world.sdf'
-    ])
+    simulation_pkg_description = get_package_share_directory('simulation')
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
 
-    # 2. Include the Gazebo Server/Client launch
-    gz_sim = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
-            ])
-        ]),
-        # Χρησιμοποιούμε λίστα για να ενώσουμε το string '-r ' με το substitution αντικείμενο
-        launch_arguments={'gz_args': [TextSubstitution(text='-r '),world_path]}.items(),
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=PathJoinSubstitution([simulation_pkg_description, 'worlds', 'drone_world.sdf']),
+        description='Gazebo world file to load',
     )
 
-    # 3. Spawn the entity (The Robot)
-    spawn_robot = Node(
-        package='ros_gz_sim',
-        executable='create',
-        arguments=[
-            '-name', 'my_robot',
-            '-topic', 'robot_description', # Uses the robot_description topic from robot_state_publisher
-            '-z', '0.5'
-        ],
-        output='screen',
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={
+            'gz_args': [TextSubstitution(text='-r '), LaunchConfiguration('world')]
+        }.items(),
     )
 
-    bridge_params = os.path.join(get_package_share_directory('simulation'), 'config', 'gz_bridge.yaml')
-    gz_bridge = Node(
+    # inclulde spawn_drone.launch.py to spawn the drone into the Gazebo world
+    spawn_drone = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(simulation_pkg_description, 'launch', 'spawn_drone.launch.py')
+        )
+    )
+
+    get_package_share_directory('ros_gz_bridge')
+    bridge_params = os.path.join(simulation_pkg_description, 'config', 'gz_bridge.yaml')
+    ros_gz_bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
-        arguments=[
-            '--ros-args',
-            '-p',
-            f'config_file:={bridge_params}',
-        ],            
+        output='screen',
+        parameters=[{'use_sim_time': True}],
+        arguments=['--ros-args', '-p', f'config_file:={bridge_params}'],
     )
 
-    ros_gz_image_bridge = Node(
-        package='ros_gz_image',
-        executable='image_bridge',
-        arguments=["/camera/image_raw"]
-    )
-
-    return LaunchDescription([
-        gz_sim, 
-        spawn_robot,
-        gz_bridge,
-        ros_gz_image_bridge,
-])
+    nodes = [world_arg, gazebo, spawn_drone, ros_gz_bridge]
+    return LaunchDescription(nodes)
